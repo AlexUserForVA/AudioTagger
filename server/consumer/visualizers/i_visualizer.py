17c16aa74987022280e42f28d9bebcaff610c2ea
Alexander Moser <35291511+AlexUserForVA@users.noreply.github.com>
2019-04-15 09:53:52 +0200

    Update i_visualizer.py

--- server/consumer/visualizers/i_visualizer.py
+++ server/consumer/visualizers/i_visualizer.py
@@ -8,3 +8,6 @@ class IVisualizer:
     def __init__(self):
         # self.buffer = deque(maxlen=BUFFER_SIZE)
         self.buffer = Queue.Queue(maxlen=BUFFER_SIZE)
+        
+    def refreshBuffer(self):
+        self.buffer = Queue.Queue(maxsize=BUFFER_SIZE)
