0b73e60d84227aa98b195f01bd92d871eaf8ede2
Alexander Moser <35291511+AlexUserForVA@users.noreply.github.com>
2019-04-15 08:57:42 +0200

    Update i_visualizer.py

--- server/consumer/visualizers/i_visualizer.py
+++ server/consumer/visualizers/i_visualizer.py
@@ -1,8 +1,10 @@
-from collections import deque
+import Queue
+# from collections import deque
 
 from server.config.config import BUFFER_SIZE
 
 class IVisualizer:
 
     def __init__(self):
-        self.buffer = deque(maxlen=BUFFER_SIZE)
+        # self.buffer = deque(maxlen=BUFFER_SIZE)
+        self.buffer = Queue.Queue(maxlen=BUFFER_SIZE)
